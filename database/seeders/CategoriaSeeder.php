25f61d42f33abaa419e30d3d44b0c0dd63209b78
Anthony Aragão <anthony.amoz.s.a@gmail.com>
2025-03-06 13:49:26 -0300

    :sparkles: feat: adicionado seeder para categorias de patrimônio

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriaSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $categorias = ['Patrimônio Material', 'Patrimônio Imaterial', 'Patrimônio Natural', 'Patrimônio Misto'];
+
+        foreach ($categorias as $categoria) {
+            DB::table('categorias')->insert([
+                'nome' => $categoria,
+            ]);
+        }
+    }
+}
